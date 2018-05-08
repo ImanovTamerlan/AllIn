c1672b11179f98ed74abc2e19b411531caf33d14
Tamerlan Imanov <imanovtamerlan@gmail.com>
2018-05-09 00:12:47 +0600

    commented code removed

--- AllIn/Auth/ResetPasswordViewController.swift
+++ AllIn/Auth/ResetPasswordViewController.swift
@@ -13,7 +13,6 @@ class ResetPasswordViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
