d3ace98174e2a641da3043b215b823b6de7cc248
Tamerlan Imanov <imanovtamerlan@gmail.com>
2018-05-08 17:13:46 +0600

    Starting
    * Created project

---
+++ AllIn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AllIn
+//
+//  Created by Tamerlan Imanov on 08.05.2018.
+//  Copyright © 2018 MarsStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
